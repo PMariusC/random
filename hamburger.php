d9ad94354c7f63ca1844cfa339495876fd1a0687
PMariusC <parfenemariuscatalin@gmail.com>
2024-06-30 18:29:43 +0300

    only pizza today

--- hamburger.php
+++ hamburger.php
@@ -1,3 +1,6 @@
+<html>
+	<title>Prod</title>
+	<body>
 <?php
 require_once 'config.php';
 session_start();
@@ -20,6 +23,20 @@ if (isset($_SESSION['loggedintoParfyApp']) && $_SESSION['loggedintoParfyApp'] ==
 
 	//all page var
 	$user_name = htmlspecialchars($_SESSION['username']);
+
+	//submit form
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$nqty = $_POST["newqty"];
+		$id_ch = $_POST["prod_id"];
+		$sqlchange ="UPDATE par_prods SET `qty`= ".$nqty." WHERE id_prod = ".$id_ch;
+		if ($conn -> query($sqlchange)) {
+			echo '<h2 style="color:ff0000;">DONE</h2>';
+			echo "<meta http-equiv='refresh' content='0'>";
+		}
+		if ($conn -> errno) {
+			echo '<h2 style="color:ff0000;">ERROR: '.$mysqli→error.'</h2>';
+		}
+	}
 	mysqli_close($conn);
 } else {
 	$actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -28,7 +45,41 @@ if (isset($_SESSION['loggedintoParfyApp']) && $_SESSION['loggedintoParfyApp'] ==
 }
 if(isset($user_name)) {
 	echo 'Hello '.$user_name.'<br>';
-	print_r($selrez);
+	echo '<div style="display:flex;flex-direction: row;">';
+	foreach($selrez[0] as $k=>$v) {
+			echo '
+				<div style="font-weight:bold;padding-right:5px;border-right:1px solid #000;margin: 0 5px 10px 0;">'.$k.'</div>
+			';
+	}
+	echo '</div>';
+	foreach($selrez as $key=>$row){
+		echo '<div style="display:flex;flex-direction: row;">';
+		foreach($row as $k=>$data) {
+			echo '
+				<div style="padding-right:5px;border-right:1px solid #000;margin: 0 5px 10px 0;">'.$data.'</div>
+			';
+		}
+		echo '</div>';
+	}
 }
-
 ?>
+
+<form action="" method="post">
+	<label for="prod_id"></label>
+	<input type="text" name="prod_id" onkeypress="return onlyNumberKey(event)" placeholder="ID Product" id="prod_id" required>
+	<label for="newqty"></label>
+	<input type="text" name="newqty" onkeypress="return onlyNumberKey(event)" placeholder="New Quantity" id="newqty" required>
+	<input type="submit" value="Submit">
+</form>
+	<script>
+		function onlyNumberKey(evt) {
+            // Only ASCII character in that range allowed
+            let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+            if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+                return false;
+            return true;
+        }
+    </script>
+
+	</body>
+</html>
